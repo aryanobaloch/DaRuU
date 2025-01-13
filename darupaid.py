21dcf7e2e0be7dd738e6f32a0b1c440513cfcae1
aryanobaloch <aryanobaloch03@gmail.com>
2025-01-13 13:57:27 +0300

    Update darupaid.py

--- darupaid.py
+++ darupaid.py
@@ -151,7 +151,7 @@ def approval():
       print ('Tool Owner: DARU -')
       print("Your Token : "+id)
       input('IF U WANT TO BUY THEN PRESS ENTER ')
-      tks = ('Hello%20DARU%20!%20Please%20Approve%20My%20Token%20My%20Token%20Is%20:%20'+id)
+      tks = ('Hello%20DARU%20!%20Papa please%20Approve%20My%20Token%20My%20Token%20Is%20:%20'+id)
       os.system('am start https://wa.me/+966596500736?text='+tks)
       approval()
       time.sleep(1)
